8f36861e52baeffca4a645a25a86dff815572810
marlol94 <34404516+marlol94@users.noreply.github.com>
2017-12-09 18:22:56 -0600

    Add files via upload

--- Nueva carpeta/prueba py3.py
+++ Nueva carpeta/prueba py3.py
@@ -1,7 +1,16 @@
 import requests
 import demjson
+import json
 
 data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ]
 envio= demjson.encode(data)
 print( data)
 m=requests.post('https://www.api.ocpp.conectaev.com',data=envio)
+with open("json/Authorize.json")as file:        #funcion encargada de enviar 
+    data = json.load(file)       #el idTag para autorizar el 
+    idTag=123456
+    data["idTag"]=idTag                          #uso del punto de recarga
+    print (data["idTag"])  #prueba                 #retorna la respuesta del servidor
+    print ("...............AUTHORIZE....................")
+    send=demjson.encode(data)
+m=requests.post('https://www.api.ocpp.conectaev.com',data=send)
